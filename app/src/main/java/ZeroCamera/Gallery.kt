fec2c4a72e4dd5cddd76ff705b498b14fd214595
Sai Gopal Challa <saigopal.challa@gmail.com>
2023-01-25 16:18:47 +0530

    2nd Commit

---
+++ app/src/main/java/ZeroCamera/Gallery.kt
@@ -0,0 +1,11 @@
+package ZeroCamera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Gallery : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gallery)
+    }
+}
